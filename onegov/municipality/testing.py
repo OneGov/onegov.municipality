e58ea19970e4968d5c3307938b1e211677fadefb
Jonas Baumann <jone@jone.ch>
2013-07-31 20:28:48 +0200

    Add testing setup.

---
+++ onegov/municipality/testing.py
@@ -0,0 +1,29 @@
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from zope.configuration import xmlconfig
+
+
+class OneGovMunicipalityLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        xmlconfig.string(
+            '<configure xmlns="http://namespaces.zope.org/zope">'
+            '  <include package="z3c.autoinclude" file="meta.zcml" />'
+            '  <includePlugins package="plone" />'
+            '  <includePluginsOverrides package="plone" />'
+            '</configure>',
+            context=configurationContext)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'onegov.municipality:default')
+
+
+MUNICIPALITY_FIXTURE = OneGovMunicipalityLayer()
+
+MUNICIPALITY_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(MUNICIPALITY_FIXTURE, ),
+    name="onegov.municipality:Integration")
